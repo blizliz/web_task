c86ca87307ccd2586c63c9f1241c8d95d86e78ac
I520950 <elizaveta.bezlutskaia@sap.com>
2020-09-09 19:21:01 +0300

    fix 1st bug

--- src/panels/Basket.js
+++ src/panels/Basket.js
@@ -33,126 +33,208 @@ const Basket = ({ match: { params: { areaId, itemId }}, foodAreas, order }) => {
 
     return [ accounting.formatNumber(result, 0, ' '), products ];
   }, [ order, item ]);
-
-  return (
-    <div className="Place">
-      <header className="Place__header">
-        <aside className="Place__trz">
-          <h1 className="Place__head">
-            <Link to="/" className="Place__logo">
-              {area.name}
-            </Link>
-          </h1>
-          <Link to="/edit" className="Place__change-tz">
+  
+  if (price > 0) {
+    return (
+        <div className="Place">
+          <header className="Place__header">
+            <aside className="Place__trz">
+              <h1 className="Place__head">
+                <Link to="/" className="Place__logo">
+                  {area.name}
+                </Link>
+              </h1>
+              <Link to="/edit" className="Place__change-tz">
+                <img
+                    alt="change-profile"
+                    src={edit}
+                />
+              </Link>
+            </aside>
+          </header>
+          <aside className="Place__restoraunt">
             <img
-              alt="change-profile"
-              src={edit}
-            />
-          </Link>
-        </aside>
-      </header>
-      <aside className="Place__restoraunt">
-        <img
-          className="Place__restoraunt-logo"
-          alt="Fastfood logo"
-          src={item.image}
-        />
-        <h2
-          className="Place__restoraunt-name"
-        >
-          {item.name}
-        </h2>
-        <p className="Place__restoraunt-type">
-          {item.description}
-        </p>
-      </aside>
-      <div className="Place__products-wrapper">
-        <ul className="Place__products">
-          {products.map(({ item, count }) => (
-            <li
-              className="Place__product"
-              key={item.id}
-            >
-              <img
-                className="Place__product-logo"
-                alt="Ordered product logo"
+                className="Place__restoraunt-logo"
+                alt="Fastfood logo"
                 src={item.image}
+            />
+            <h2
+                className="Place__restoraunt-name"
+            >
+              {item.name}
+            </h2>
+            <p className="Place__restoraunt-type">
+              {item.description}
+            </p>
+          </aside>
+          <div className="Place__products-wrapper">
+            <ul className="Place__products">
+              {products.map(({ item, count }) => (
+                  <li
+                      className="Place__product"
+                      key={item.id}
+                  >
+                    <img
+                        className="Place__product-logo"
+                        alt="Ordered product logo"
+                        src={item.image}
+                    />
+                    <h3
+                        className="Place__product-name"
+                    >
+                      {item.name}
+                    </h3>
+                    <p
+                        className="Place__product-price"
+                    >
+                      Цена: {item.price}
+                    </p>
+                    <p
+                        className="Place__product-count"
+                    >
+                      x{count}
+                    </p>
+                  </li>
+              ))}
+            </ul>
+            <Link
+                className="Place__change-product"
+                to={`/place/${areaId}/${itemId}`}
+            >
+              Изменить
+            </Link>
+          </div>
+          <div className="Place__choice">
+            <h3>Время:</h3>
+            <div className="Place__choice-item">
+              <span>Как можно быстрее</span>
+              <Checkbox
+                  checked={faster}
+                  onToggle={() => {
+                    if (faster) {
+                      setFaster(false);
+                    } else {
+                      setTime('');
+                      setFaster(true);
+                    }
+                  }}
               />
-              <h3
-                className="Place__product-name"
-              >
-                {item.name}
-              </h3>
-              <p
-                className="Place__product-price"
-              >
-                Цена: {item.price}
-              </p>
-              <p
-                className="Place__product-count"
-              >
-                x{count}
-              </p>
-            </li>
-          ))}
-        </ul>
-        <Link
-          className="Place__change-product"
-          to={`/place/${areaId}/${itemId}`}
-        >
-          Изменить
-        </Link>
-      </div>
-      <div className="Place__choice">
-        <h3>Время:</h3>
-        <div className="Place__choice-item">
-          <span>Как можно быстрее</span>
-          <Checkbox 
-            checked={faster} 
-            onToggle={() => {
-              if (faster) {
-                setFaster(false);
-              } else {
-                setTime('');
-                setFaster(true);
-              }
-            }}
-          />
+            </div>
+            <div className="Place__choice-item">
+              <span>Назначить</span>
+              <input
+                  value={time}
+                  onFocus={() => {
+                    setFaster(false);
+                  }}
+                  onChange={event => {
+                    setFaster(false);
+                    setTime(event.target.value);
+                  }}
+                  onBlur={() => {
+                    if (time) {
+                      setFaster(false);
+                    }
+                  }}
+              />
+            </div>
+            <div className="Place__choice-item">
+              <h3>С собой</h3>
+              <Checkbox checked={selfService} onToggle={() => setSelfService(!selfService)} />
+            </div>
+            <div className="Place__choice-item">
+              <h3>На месте</h3>
+              <Checkbox checked={!selfService} onToggle={() => setSelfService(!setSelfService)} />
+            </div>
+          </div>
+          <footer className="Place__footer">
+            <Link to={`/order/${area.id}/${item.id}`} className="Place__order">
+              Оплатить {price}
+            </Link>
+          </footer>
         </div>
-        <div className="Place__choice-item">
-          <span>Назначить</span>
-          <input
-            value={time}
-            onFocus={() => {
-              setFaster(false);
-            }}
-            onChange={event => {
-              setFaster(false);
-              setTime(event.target.value);
-            }}
-            onBlur={() => {
-              if (time) {
-                setFaster(false);
-              }
-            }}
-          />
+    );
+  } else {
+    return (
+        <div className="Place">
+          <header className="Place__header">
+            <aside className="Place__trz">
+              <h1 className="Place__head">
+                <Link to="/" className="Place__logo">
+                  {area.name}
+                </Link>
+              </h1>
+              <Link to="/edit" className="Place__change-tz">
+                <img
+                    alt="change-profile"
+                    src={edit}
+                />
+              </Link>
+            </aside>
+          </header>
+          <aside className="Place__restoraunt">
+            <img
+                className="Place__restoraunt-logo"
+                alt="Fastfood logo"
+                src={item.image}
+            />
+            <h2
+                className="Place__restoraunt-name"
+            >
+              {item.name}
+            </h2>
+            <p className="Place__restoraunt-type">
+              {item.description}
+            </p>
+          </aside>
+          <div className="Place__products-wrapper">
+            <ul className="Place__products">
+              {products.map(({ item, count }) => (
+                  <li
+                      className="Place__product"
+                      key={item.id}
+                  >
+                    <img
+                        className="Place__product-logo"
+                        alt="Ordered product logo"
+                        src={item.image}
+                    />
+                    <h3
+                        className="Place__product-name"
+                    >
+                      {item.name}
+                    </h3>
+                    <p
+                        className="Place__product-price"
+                    >
+                      Цена: {item.price}
+                    </p>
+                    <p
+                        className="Place__product-count"
+                    >
+                      x{count}
+                    </p>
+                  </li>
+              ))}
+            </ul>
+            <Link
+                className="Place__change-product"
+                to={`/place/${areaId}/${itemId}`}
+            >
+              Изменить
+            </Link>
+          </div>
+          <p>Вы ничего не заказали</p>
+          <Link
+              to={item.link}
+          >
+            <h3 className="Home__food-name">
+              Вернуться в ресторан
+            </h3>
+          </Link>
         </div>
-        <div className="Place__choice-item">
-          <h3>С собой</h3>
-          <Checkbox checked={selfService} onToggle={() => setSelfService(!selfService)} />
-        </div>
-        <div className="Place__choice-item">
-          <h3>На месте</h3>
-          <Checkbox checked={!selfService} onToggle={() => setSelfService(!setSelfService)} />
-        </div>
-      </div>
-      <footer className="Place__footer">
-        <Link to={`/order/${area.id}/${item.id}`} className="Place__order">
-          Оплатить {price}
-        </Link>
-      </footer>
-    </div>
-  );
+    );
+  }
 };
 
 export default withRouter(Basket);
